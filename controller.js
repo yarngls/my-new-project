01f55596073e4ff84289f3490670f7a0b7e8aaa6
yarngls <javaphyton1987@gmail.com>
2017-07-11 15:13:54 -0100

    update controller

--- controller.js
+++ controller.js
@@ -1,4 +1,5 @@
 var app = angulr.module("myApp",[]);
 app.controller("myController",function ($scope,$http,$routeProvider) {
 		$scope.tittle = "Ola meu caro";
+		$scope.name="Gilson Silva Santos";
 });
